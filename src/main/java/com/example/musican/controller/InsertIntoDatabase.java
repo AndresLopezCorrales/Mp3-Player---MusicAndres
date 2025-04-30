603e88e5f0ac09c56f1c5b92bc7d4bc044fb1580
Andrés López Corrales <andreslopezcorrales326@gmail.com>
2025-04-30 10:36:44 -0700

    Functions for adding new music to the database to be reproduce in the mp3player. no functions yet

---
+++ src/main/java/com/example/musican/controller/InsertIntoDatabase.java
@@ -0,0 +1,14 @@
+package com.example.musican.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class InsertIntoDatabase {
+
+
+}
